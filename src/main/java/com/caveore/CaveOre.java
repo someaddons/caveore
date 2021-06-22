51e95655016ee45334528a8b934d4ae113f1aede
someaddons <someaddons@github.com>
2021-06-23 00:10:00 +0200

    v1.5

--- src/main/java/com/caveore/CaveOre.java
+++ src/main/java/com/caveore/CaveOre.java
@@ -3,9 +3,13 @@ package com.caveore;
 import com.caveore.config.ConfigValues;
 import com.caveore.config.Configuration;
 import com.caveore.event.ModEventHandler;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -19,6 +23,8 @@ public class CaveOre
 
     public CaveOre()
     {
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+
         config = new Configuration();
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         Mod.EventBusSubscriber.Bus.MOD.bus().get().register(ModEventHandler.class);
