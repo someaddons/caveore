69502ec2b53d8ce505c4c570c8fb6f59dd5ae9ad
someaddons <someaddons@github.com>
2025-01-08 21:33:17 +0100

    3.9: Fix mixin crash

--- src/main/java/com/caveore/mixin/OreVeinMixin.java
+++ src/main/java/com/caveore/mixin/OreVeinMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(OreVeinifier.class)
 public class OreVeinMixin
 {
-    @Redirect(method = "lambda$create$0", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/RandomSource;nextFloat()F"))
+    @Redirect(method = "m_209660_", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/RandomSource;m_188501_()F"), remap = false)
     private static float adjustDensity(final RandomSource instance)
     {
         return (float) (instance.nextFloat() * 1 / (CaveOre.config.getCommonConfig().oreVeinDensityModifier + 0.00001));
